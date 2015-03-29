9de7ebe9a2b4eeca05457e00276aac452983ef00
Keita Iseki <keita.iseki@gmail.com>
2015-03-29 23:40:18 +0900

    support bootstrap.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 require("jquery");
 require("underscore");
 require("bootstrap");
+require("bootstrap.css");
+require("bootstrap-multiselect");
+require("bootstrap-multiselect.css");
 
 require("./style.css");
 document.write(require("./content.js"));
 
 $(function() { 
     $("#content").text(_.map([1,2,3], function(x) { return x * 2; }).join());
+    $("#multiselect").multiselect({
+        enableFiltering: true
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,45 @@
 var path = require("path");
 var webpack = require("webpack");
+
 module.exports = {
     entry: "./index.js",
     output: {
-	path: __dirname,
+	path: path.join(__dirname, "dist/"),
 	filename: "bundle.js"
     },
     devtool: 'inline-source-map',
     module: {
 	loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+	    // for bootstrap.css
+            { test: /\.woff$/, loader: "url-loader?mimetype=application/font-woff" },
+            { test: /\.woff2$/, loader: "url-loader?mimetype=application/font-woff2" },
+            { test: /\.ttf$/, loader: "url-loader?mimetype=application/x-font-ttf" },
+            { test: /\.eot$/, loader: "url-loader?mimetype=application/vnd.ms-fontobject" },
+            { test: /\.svg$/, loader: "url-loader?mimetype=image/svg+xml" },
+	    // for bootstrap-multiselect
+            { test: /jquery\.js$/, loader: "expose?jQuery" }
         ]
     },
     resolve: {
+        alias: {
+	    "bootstrap": path.join(__dirname, "bower_components/bootstrap/dist/js/bootstrap.js"),
+	    "bootstrap.css": path.join(__dirname, "bower_components/bootstrap/dist/css/bootstrap.css"),
+	    "bootstrap-multiselect": path.join(__dirname, "bower_components/bootstrap-multiselect/dist/js/bootstrap-multiselect.js"),
+	    "bootstrap-multiselect.css": path.join(__dirname, "bower_components/bootstrap-multiselect/dist/css/bootstrap-multiselect.css"),
+        },
         modulesDirectories: [
             path.join(__dirname, "bower_components")
         ]
     },
     plugins: [
         new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
         ),
         new webpack.ProvidePlugin({
 	    $: "jquery",
-	    jQuery: "jquery",
+            // multiselect が window.jQuery を参照しているため、expose-loader で jQuery を設定している
+            // jQuery: "jquery",
             _: "underscore"
         })
     ]
